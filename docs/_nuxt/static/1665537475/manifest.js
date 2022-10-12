270f40c6b73c0ae68c38b33feb448d90478752e3
jooohyunpark <jhp527@nyu.edu>
2022-10-11 21:18:01 -0400

    update

---
+++ docs/_nuxt/static/1665537475/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fproject-4","\u002Fproject-7","\u002Fproject-5","\u002Fabout","\u002Fproject-1","\u002Fproject-6","\u002Fproject-9","\u002Fproject-8","\u002Fproject-3","\u002Fproject-2"]})
